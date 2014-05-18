65435b06dc66456fe6521fdfdb7b9dc39ebcf867
Bin Zhuo <zhuob@zhuob-VirtualBox.(none)>
2014-05-18 13:44:48 -0700

    some code from Bin

---
+++ cluster.R
@@ -0,0 +1,168 @@
+
+leaf <- read.csv("leaf.csv", header=T)
+mydata <- leaf[, c(-1,-2)]
+mydata <- scale(mydata)
+
+# seed is set to use the same training and 
+# validation datasets and make the two methods comparable.
+set.seed(100)
+id <- sample(1:dim(mydata)[1], 300)
+training <- mydata[id, ]  # training data
+validation <- mydata[-id,] # validation data
+
+
+# K-Means Cluster Analysis
+
+# Determine number of clusters
+wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
+for (i in 2:15) wss[i] <- sum(kmeans(mydata, 
+                                     centers=i)$withinss)
+plot(1:15, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares")
+
+
+fit <- kmeans(mydata, 12) # 5 cluster solution
+# get cluster means 
+aggregate(mydata,by=list(fit$cluster),FUN=mean)
+# append cluster assignment
+ mydata <- data.frame(mydata, fit$cluster)
+
+library(cclust)
+mod1 <- cclust(training, centers= 8, iter.max = 50,
+               dist = "manhattan",method="kmeans")
+
+km.pred <- predict(mod1, validation)
+names(km.pred)
+km.pred$cluster
+
+# Cluster Plot against 1st 2 principal components
+
+# vary parameters for most readable graph
+library(cluster) 
+clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE, 
+         labels=2, lines=0)
+# Centroid Plot against 1st 2 discriminant functions
+library(fpc)
+plotcluster(mydata, fit$cluster)
+
+
+
+
+
+
+## model based clustering  Mclust
+
+leaf <- read.csv("leaf.csv", header=T)
+mydata1 <- leaf[, c(-1,-2)]
+mydata1 <- scale(mydata1)
+
+# seed is set to use the same training and 
+# validation datasets and make the two methods comparable.
+set.seed(100)
+id <- sample(1:dim(mydata)[1], 300)
+training <- mydata1[id, ]  # training data
+validation <- mydata1[-id,] # validation data
+
+
+library(mclust)
+
+fit <- Mclust(training, G= 1:18)
+names(fit)
+fit$G # optimal number of clustering
+
+fit$parameters
+
+## prediction
+pre <- predict(fit, validation)
+pre$classification
+plot(fit, what="classification" )
+
+
+## how to visualize the results?
+
+
+##  Hierarchical Clustering
+# examples from http://www.stat.berkeley.edu/classes/s133/Cluster2a.html
+library(protoclust)
+hc <- protoclust(dist(training))
+names(hc)
+
+plot(hc, main = "default from hclust")
+
+k <- 8
+group.8 <- protocut(hc, k) # cut the tree into 8 groups
+h <- hc$height[dim(training)[1]-k]
+
+# plot dendrogram and show cut
+plotwithprototypes(hc, imerge=group.8$imerge, col=2)
+abline(h=h, lty=2)
+predict(hc, validation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# generate some data:
+set.seed(1)
+n <- 100
+p <- 2
+x <- matrix(rnorm(n * p), n, p)
+rownames(x) <- paste("A", 1:n, sep="")
+d <- dist(x)
+# perform minimax linkage clustering:
+hc <- protoclust(d)
+# cut the tree to yield a 10-cluster clustering:
+k <- 10 # number of clusters
+cut <- protocut(hc, k=k)
+h <- hc$height[n - k]
+# plot dendrogram (and show cut):
+plotwithprototypes(hc, imerge=cut$imerge, col=2)
+abline(h=h, lty=2)
+# get the prototype assigned to each point:
+pr <- cut$protos[cut$cl]
+# find point farthest from its prototype:
+dmat <- as.matrix(d)
+ifar <- which.max(dmat[cbind(1:n, pr[1:n])])
+# note that this distance is exactly h:
+stopifnot(dmat[ifar, pr[ifar]] == h)
+# since this is a 2d example, make 2d display:
+plot(x, type="n")
+points(x, pch=20, col="lightblue")
+lines(rbind(x[ifar, ], x[pr[ifar], ]), col=3)
+points(x[cut$protos, ], pch=20, col="red")
+text(x[cut$protos, ], labels=hc$labels[cut$protos], pch=19)
+tt <- seq(0, 2 * pi, length=100)
+for (i in cut$protos) {
+  lines(x[i, 1] + h * cos(tt), x[i, 2] + h * sin(tt))
+}
